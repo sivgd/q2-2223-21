206f4b7fae8425d176207765edc0f8dc7b796efb
MUKILTEO\1710152 <Riley05Park@gmail.com>
2022-12-07 13:28:49 -0800

    Updated my scene to represent iteration one of the level

---
+++ Q2-Team-21/Assets/Riley/FaceCamera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    public GameObject player;
+    public float DegreesRotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 VectorToPlayer = new Vector3(player.transform.position.x - transform.position.x, 0, player.transform.position.z - transform.position.z).normalized;
+
+        DegreesRotate = Mathf.Atan2(VectorToPlayer.x, VectorToPlayer.z) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(90, DegreesRotate, 0);
+
+    }
+}
