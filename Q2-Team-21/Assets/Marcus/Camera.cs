6457f8be02e6d8a6c37ca3a5cfa357c09a6e63f6
MUKILTEO\1709603 <garmanmt@gmail.com>
2022-12-09 13:25:05 -0800

    character movement

--- Q2-Team-21/Assets/Marcus/Camera.cs
+++ Q2-Team-21/Assets/Marcus/Camera.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    //Camera
     private GameObject PlayerCam;
     private Transform PlayerCamera;
     public float Sensitivity;
     private Vector2 Sensitivities;
     private Vector2 XYRotation;
 
+    //Raycast
+
+    private LayerMask InteractablesLayerMask;
+    private GameObject LookingAtObj;
+
     void Start()
     {
+        //Camera
+
         Cursor.lockState = CursorLockMode.Locked;
         PlayerCam = GameObject.FindGameObjectWithTag("MainCamera");
         PlayerCamera = PlayerCam.transform;
 
         Sensitivities.x = Sensitivity;
         Sensitivities.y = Sensitivity;
+
+        //Raycast
+
+        InteractablesLayerMask = LayerMask.GetMask("Interactables");
     }
 
     void Update()
     {
+        //Camera
+
         Vector2 MouseInput = new Vector2
         {
             x = Input.GetAxis("Mouse X"),
@@ -36,5 +50,30 @@ public class Camera : MonoBehaviour
 
         PlayerCam.transform.eulerAngles = new Vector3(XYRotation.x, XYRotation.y, 0f);
         PlayerCamera.localEulerAngles = new Vector3(XYRotation.x, XYRotation.y, 0f);
+
+        //Raycast
+
+        var ray = new Ray(origin: this.transform.position, direction: this.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, InteractablesLayerMask))
+        {
+            LookingAtObj = hit.transform.gameObject;
+            if (LookingAtObj.tag == "")
+            {
+
+            }
+            if (LookingAtObj.tag == "")
+            {
+
+            }
+            if (LookingAtObj.tag == "")
+            {
+
+            }
+        }
+        else
+        {
+            LookingAtObj = null;
+        }
     }
 }
