4ccd67735d99befdd1d831825493135898d6062a
MUKILTEO\1710219 <1710219@mulkilteo.wednet.edu>
2022-12-13 11:26:10 -0800

    Spert

---
+++ Q2-Team-21/Assets/Adrian's Folder KEEP OUT!!!!/Scripts/SpertShoot.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpertShoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bullet;
+    public Transform bulletPos;
+    public float time;
+    private float timer;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > time)
+        {
+            timer = 0;
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+}
