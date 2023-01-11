1d4629d03209d0f6ea4125a7efd41ad26d656efe
MUKILTEO\1710152 <Riley05Park@gmail.com>
2023-01-11 13:23:15 -0800

    AAAAA

---
+++ Q2-Team-21/Assets/Riley/Scripts/WhatDirection.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatDirection : MonoBehaviour
+{
+    public float DirectionFacing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DirectionFacing = transform.rotation.eulerAngles.y;
+        //Debug.Log(transform.rotation.eulerAngles.y);
+    }
+}
