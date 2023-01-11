2b84250a8b46afc9b4da93582766722e780f92b1
breakingit32 <75185545+breakingit32@users.noreply.github.com>
2023-01-11 11:22:43 -0800

    ghhtygfftxs

--- Q2-Team-21/Assets/Marcus/C#/CharacterControllerScript.cs
+++ Q2-Team-21/Assets/Marcus/C#/CharacterControllerScript.cs
@@ -1,7 +1,7 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
+
 using UnityEngine;
 
 public class CharacterControllerScript : MonoBehaviour
