c43c5e1b4750045062615ef4ae11038ee79bb568
MUKILTEO\1710152 <Riley05Park@gmail.com>
2023-01-24 12:13:43 -0800

    UI is weird

--- Q2-Team-21/Assets/Riley/Scripts/FadeIn.cs
+++ Q2-Team-21/Assets/Riley/Scripts/FadeIn.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class FadeIn : MonoBehaviour
 {
-
-    //private Image Img;
+    public Image img;
+    
     // Start is called before the first frame update
     void Start()
     {
         
+        img = GetComponent<Image>();
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //Debug.Log(Img.tintColor.a.ToString());
-        //if (Img.tintColor.a > 0) Img.tintColor = new Color(0, 0, 0, Img.tintColor.a - 1);
+        
+        img.tintColor = Color.white;
+        Debug.Log(img.tintColor.a);
+       
+        this.GetComponent<Image>().tintColor = new Color(0, 0, 0, img.tintColor.a-5);
+        
     }
 }
