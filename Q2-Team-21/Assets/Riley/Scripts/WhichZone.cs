90491015707c01e7c457963f12ae3bf171b612ac
MUKILTEO\1710152 <Riley05Park@gmail.com>
2023-01-31 12:51:45 -0800

    got some music implementation in my test scene, adding to final scene tommorow

--- Q2-Team-21/Assets/Riley/Scripts/WhichZone.cs
+++ Q2-Team-21/Assets/Riley/Scripts/WhichZone.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class WhichZone : MonoBehaviour
 {
-    public GameObject player;
+    private GameObject player;
+    public bool IsInSpikeZone;
+    public GameObject SpikeTheme;
+    public bool IsInCoralArea;
+    public GameObject CoralTheme;
+    public bool InMainArea;
+    public GameObject MainTheme;
+    public GameObject CoralZoneMarker;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("PlayerBoat");
+        IsInSpikeZone = false;
+        IsInCoralArea = false;
+        InMainArea = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.x > 100 && player.transform.position.z > -180) IsInSpikeZone = true;
+        if(player.transform.position.x <= 100 || player.transform.position.z <= -180) IsInSpikeZone = false;
+
+        if (Vector3.Distance(player.transform.position, CoralZoneMarker.transform.position) < 300) IsInCoralArea = true;
+        else IsInCoralArea = false;
+
+        if (IsInSpikeZone == false && IsInCoralArea == false) InMainArea = true;
+        else if (IsInSpikeZone == true || IsInCoralArea == true) InMainArea = false;
+
+        CoralTheme.SetActive(IsInCoralArea);
+        MainTheme.SetActive(InMainArea);
+        SpikeTheme.SetActive(IsInSpikeZone);
+
+
+
+
     }
 }
