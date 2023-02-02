e36a4e0e2389f8713911dbf4fef8c1384a8932a2
MUKILTEO\1710152 <Riley05Park@gmail.com>
2023-02-02 12:28:46 -0800

    updated end cutscene

--- Q2-Team-21/Assets/EndCutscene.cs
+++ Q2-Team-21/Assets/EndCutscene.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndCutscene : MonoBehaviour
 {
+    public Image img;
     public GameObject BoatCam;
     public GameObject CutsceneCam;
     public BoatEngine Engine;
-
+    private bool hasFinished = false;
     void Start()
     {
         
@@ -15,15 +17,26 @@ public class EndCutscene : MonoBehaviour
 
     void Update()
     {
-        
+        if (img.color.a < 1 && hasFinished == true)
+        {
+            img.color = new Color(255, 255, 255, img.color.a + 0.001f);
+
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
 
+        if (other.tag == "PlayerBoat")
+        {
+
+
             BoatCam.SetActive(false);
             CutsceneCam.SetActive(true);
             Engine.Finished = true;
+            hasFinished = true;
+
+        }    
         
     }
 }
